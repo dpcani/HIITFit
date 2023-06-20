e39a5bb048ce8561fdc25e3c9ef169d785309578
David de la Puente <david.puente@marbleinteractive.com>
2023-06-20 10:43:42 +0200

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Marble Interactive SL on 20/6/23.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
